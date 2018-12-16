55d847640aca55e3d93ce0c18cb32668107ceece
unknown <encianhoratiu@gmail.com>
2018-12-16 10:55:30 +0200

    ads

--- test/login.test.js
+++ test/login.test.js
@@ -1,64 +1,54 @@
 require("chromedriver");
-    var { describe, it, after, before} = require('selenium-webdriver/testing');
-    var webdriver = require('selenium-webdriver'),
+var { describe, it, after, before } = require('selenium-webdriver/testing');
+var webdriver = require('selenium-webdriver'),
+    By = webdriver.By,
+    until = webdriver.until,
     chrome = require('selenium-webdriver/chrome'),
     o = new chrome.Options();
-    o.addArguments("start-maximized"),
+o.addArguments("start-maximized"),
     o.addArguments('disable-infobars');
-  
-    var utils = require('../utils/constants');
-    var Home = require("../lib/home_page");
-    var LoginPage = require("../lib/login_page");
-    var HeaderPage = require("../lib/header_page");
-    var homePage, loginPage, headerPage;
-    var driver;
 
+var utils = require('../utils/constants');
+var Home = require("../lib/home_page");
+var LoginPage = require("../lib/login_page");
+var HeaderPage = require("../lib/header_page");
+var homePage, loginPage, headerPage;
+var driver;
 
- 
-    describe('library app scenario', function(){
-        this.timeout(999999);
-        
-        beforeEach(function(){
-            driver = new webdriver.Builder().withCapabilities(webdriver.Capabilities.chrome()).setChromeOptions(o).build();
-            homePage = new Home(driver);
-            loginPage = new LoginPage(driver);
-            headerPage = new HeaderPage(driver);
-        });
+describe('library app scenario', function () {
+    this.timeout(999999);
 
-        afterEach(function(){
-            // homePage.quit();
-        });
-        
-        it('Login Test', function(){
-            homePage.open();
-            homePage.openLoginForm();
-            loginPage.login(utils.email, utils.password);
-            headerPage.headerIsPresent();
-            headerPage.selectHeaderOption("ITEMS");
-            headerPage.selectHeaderOption("USERS");
-            headerPage.selectHeaderOption("ITEMS");
-            headerPage.selectHeaderOption("USERS");
-            headerPage.selectHeaderOption("ITEMS");
-            headerPage.selectHeaderOption("USERS");
-            headerPage.selectHeaderOption("ITEMS");
-            headerPage.selectHeaderOption("USERS");
-            headerPage.selectHeaderOption("ITEMS");
-            headerPage.selectHeaderOption("USERS");
-            headerPage.selectHeaderOption("ITEMS");
-            headerPage.selectHeaderOption("USERS");
-            headerPage.selectHeaderOption("ITEMS");
-            headerPage.selectHeaderOption("USERS");
-            headerPage.selectHeaderOption("ITEMS");
-            headerPage.selectHeaderOption("USERS");
-            headerPage.selectHeaderOption("ITEMS");
-            headerPage.selectHeaderOption("USERS");
-            headerPage.selectHeaderOption("ITEMS");
-            headerPage.selectHeaderOption("USERS");
-        });
+    beforeEach(function () {
+        driver = new webdriver.Builder().withCapabilities(webdriver.Capabilities.chrome()).setChromeOptions(o).build();
+        homePage = new Home(driver);
+        loginPage = new LoginPage(driver);
+        headerPage = new HeaderPage(driver);
+    });
 
+    afterEach(function () {
+        // homePage.quit();
+    });
 
+    it('Login Test', function () {
+        homePage.open();
+        homePage.openLoginForm();
+        loginPage.login(utils.email, utils.password);
+        console.log(loginPage.check(By.css("div[class='ww-loading']")));
+        // driver.sleep(5000);
+        console.log(loginPage.check(By.css("div[class='ww-loading hide']")));
+        // headerPage.headerIsPresent();
+        // headerPage.selectHeaderOption("ITEMS");
+        // headerPage.selectHeaderOption("USERS");
+        // headerPage.selectHeaderOption("ITEMS");
+        // headerPage.selectHeaderOption("USERS");
+        // headerPage.selectHeaderOption("ITEMS");
+        // headerPage.selectHeaderOption("USERS");
+        // headerPage.selectHeaderOption("ITEMS");
+        // headerPage.selectHeaderOption("USERS");
     });
 
 
-   
- 
+});
+
+
+
