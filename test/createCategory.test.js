dae9a53402649d25cefc5cff0b4800b8a74eeefa
Suzana Garam <suzana.garam@evozon.com>
2019-01-22 06:08:07 -0800

    latest changes

--- test/createCategory.test.js
+++ test/createCategory.test.js
@@ -30,20 +30,27 @@ describe('Create category', function () {
     });
 
     afterEach(function () {
-        homePage.quit();
+        // homePage.quit();
     });
 
-    it('I should be able to ceate a category', function () {
+    it('I should be able to ceate a category', async  function () {
         let categoryName = categoryFactory().name;
-        homePage.open();
-        homePage.openLoginForm();
-        loginPage.login(utils.email, utils.password);
-        headerPage.selectHeaderOption('USERS');
-        headerPage.selectHeaderOption('BOOKINGS');
-        headerPage.selectHeaderOption('ITEMS');
-        itemsPage.selectAction('Add Category');
-        itemsPage.createCategory(categoryName);
-       // itemsPage.categoryIsPresent(categoryName);
+        await homePage.open();
+        console.log("Test 1");
+        await homePage.openLoginForm();
+        console.log("Test 2");
+        await loginPage.login(utils.email, utils.password);
+        console.log("Test 3");
+        await headerPage.selectHeaderOption('ITEMS');
+        console.log("Test 4");
+        // headerPage.selectHeaderOption('BOOKINGS');
+        // headerPage.selectHeaderOption('ITEMS');
+        await itemsPage.selectAction('Add Category');
+        console.log("Test 5");
+        await itemsPage.createCategory(categoryName);
+        console.log("Test 6");
+        await itemsPage.categoryIsPresent(categoryName);
+        console.log("Test 7");
     });
 
 
